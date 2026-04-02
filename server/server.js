fdd674045a3e2f7be4f3e30eee8ab58c3bfb12d2
patthanasakbank-pixel <patthanasakbank@gmail.com>
2026-04-02 19:24:09 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require(“express”);
+const express = require("express")
 const http = require(“http”);
 const { Server } = require(“socket.io”);
 const path = require(“path”);
@@ -360,4 +360,10 @@ io.emit(“gameReset”);
 
 server.listen(3000, () => {
 console.log(“ Night Has Come server running on http://localhost:3000”);
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });

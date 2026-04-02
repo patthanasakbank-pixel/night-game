e4150b7c755a524c3e92b747dba77b47c3975fea
patthanasakbank-pixel <patthanasakbank@gmail.com>
2026-04-02 18:59:42 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -358,8 +358,7 @@ io.emit(“gameReset”);
 });
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+    console.log(`🌑 Server running on port ${PORT}`);
 });
